b740f1751c0835659681540f3780ac5a0e03dc64
Gabriel Maciel <gabrielmaciel2310@gmail.com>
2021-04-26 14:58:26 -0400

    Initial Commit

---
+++ GarageBandProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GarageBandProject
+//
+//  Created by Gabriel de Oliveira Maciel on 26/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
